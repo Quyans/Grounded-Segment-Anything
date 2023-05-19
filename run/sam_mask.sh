8953a72ee1f222352bf3f25526b403f4abdc9d73
Quyans <648224071@qq.com>
2023-05-19 10:09:49 +0800

    add SAM_demo_file

---
+++ run/sam_mask.sh
@@ -0,0 +1,24 @@
+
+
+# python grounded_sam_demo.py   \
+#     --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py   \
+#     --grounded_checkpoint groundingdino_swint_ogc.pth   \
+#     --sam_checkpoint sam_vit_h_4b8939.pth   \
+#     --input_image assets/nerf/hotdog.png   \
+#     --output_dir "outputs"   \
+#     --box_threshold 0.3   \
+#     --text_threshold 0.25   \
+#     --text_prompt "plate"   \
+#     --device "cuda"
+
+
+python grounded_sam_Filefolder.py   \
+    --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py   \
+    --grounded_checkpoint groundingdino_swint_ogc.pth   \
+    --sam_checkpoint sam_vit_h_4b8939.pth   \
+    --input_folder assets/nerf/hotdog/val   \
+    --output_dir "assets/nerf/hotdog/val/masks"   \
+    --box_threshold 0.3   \
+    --text_threshold 0.25   \
+    --text_prompt "plate"   \
+    --device "cuda"
